50dbe58fdf4fa7a527006f7eb96161db9a07e038
youssef amgad <55314119+yousseffamgadd@users.noreply.github.com>
2025-05-13 20:59:51 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'login_screen.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart'; 
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
